1bbb4f790441b0acdd83c32e07458af63a706bdb
Jagdish <jmhulsure@gmail.com>
2019-05-10 13:40:41 +0530

    kofluence task code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+pet : any;
   constructor(public navCtrl: NavController) {
-
+     
+      this.pet = "kittens";
+ 
   }
+  
 
 }
